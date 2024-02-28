581f26ae3a7651e811622d8c1378a08a0f7b8f9c
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2024-02-28 09:42:13 -0600

    Initial commit

---
+++ TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private TempManagerContext context;
+        public ValidationController(TempManagerContext cxt) => context = cxt;
+
+        public JsonResult CheckDate(string date)
+        {
+            DateTime convertedDate = DateTime.Parse(date);
+            string errorMsg = "Date is already in the database";
+
+            var temp = context.Temps.FirstOrDefault(x => x.Date == convertedDate);
+
+            if (temp != null)
+            {
+                TempData["okDate"] = true;
+                return Json(errorMsg);
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+        public static string DateExists(TempManagerContext context, string date)
+        {
+            string msg = "";
+            DateTime convertedDate = DateTime.Parse(date);
+            if (!string.IsNullOrEmpty(date))
+            {
+                var temp = context.Temps.FirstOrDefault(x => x.Date == convertedDate);
+                if(temp != null)
+                {
+                    msg = "Duplicate Date";
+                }
+            }
+            return msg;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
